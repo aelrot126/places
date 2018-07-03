556bf0a4655089ef58e5862c6e59b33af6ed895d
Jaden <yjian126@hotmail.com>
2018-07-03 12:53:31 +0800

    03072018

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const server = express ();
 const path =require('path');
 const filemgr = require('./filemgr');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 server.use(bodyParser.urlencoded({extended:true}));
 server.set('view engine', 'hbs');
